70ee8c2558c6a5067f29ccee14bc6ec29bb3b1e5
bprinty <bprinty@gmail.com>
2020-03-13 20:31:31 -0500

    Finished processing logic

--- src/index.js
+++ src/index.js
@@ -2,61 +2,201 @@
  * Main entry point for module
  */
 
-// import fs from 'fs';
+
+// imports
 import jsdoc from 'jsdoc-api';
-// import jsdoc from 'jsdoc-parse';
-import doxdox from 'doxdox';
+import fs from 'fs';
+import Token from 'markdown-it/lib/token';
 
-import Plugin from 'markdown-it-regexp';
+// read style for plugin (bundled with package)
+let CSS = fs.readFileSync(__dirname + '/index.css', 'utf-8');
+CSS = `\n\n<style>\n${CSS}\n</style>\n\n`;
 
-
-// export const autodoc = Plugin(
-//   /\w/,
-//   function (match, utils) {
-//     console.log('match', match);
-//     const [path, ...modules] = match[1].trim().split(/[ ,;]/);
-//     return `<h1>${path}<h1><p>${modules}</p>`;
-//   }
-// );
-
-function document(path, ...items) {
-  const data = jsdoc.explainSync({
-    files: [path],
-  }).filter(item => item.comment);
-  // const data = jsdoc([{
-  //   files: [path],
-  // }]);
-  // console.log(data[2].params);
-  // console.log(data[2].params[0].type.names);
-  // const elements = data
-  //   .filter(item => item.comment);
-  // elements.map(element => {
-  //   console.log(element.meta.range);
-  // });
-  // console.log(elements[0].meta.code);
-  // console.log(elements[0].meta.range);
-  // // console.log(items.length === 0);
-  // console.log(items.includes(elements[0].name));
-  //   // .filter(item => items.length === 0 || items.includes(item.name));
-  // console.log(elements);
-  return `<h1>${path}<h1><p>${items}</p>`;
+/**
+ * Return badge class for specific object type.
+ *
+ * @param type - Object type to document.
+ */
+function badgeClass(type) {
+  const data = { class: 'tip', function: 'error', const: 'warning', member: 'warning' };
+  return (type in data) ? data[type] : 'tip';
 }
 
 
+/**
+ * Generate html formatting for single parameter.
+ *
+ * @param param - Object with param data structure.
+ */
+function formatParam(param) {
+  const name = param.name ? `<code>${param.name}</code>` : '';
+  const type = param.type ? ' (' + param.type.names.map(x => `<em>${x}</em>`).join(', ') + ')': '';
+  const desc = param.description ? ` - ${param.description}` : '';
+  return `${name}${type}${desc}`;
+}
+
+
+/**
+ * Generate formatted html for single component.
+ *
+ * @param data - Structured data to generate html from.
+ */
+function html(data, nested) {
+  nested = nested || false;
+  let result = [];
+  let cls = badgeClass(data.type);
+  let call = `${data.name}`;
+  if (['class', 'function'].includes(data.type)) {
+    call += `(`;
+    if (data.params) {
+      call += data.params.map(x => x.name).join(', ');
+    }
+    call += `)`;
+  }
+  result.push('<div class="autodoc">');
+
+  // header
+  const htag = nested ? 'h4' : 'h3';
+  result.push(`<${htag} id="${data.name}">`);
+  result.push(` <a href="#${data.name}" class="header-anchor">#</a>`);
+  result.push(` <span class="badge ${cls}" style="vertical-align: top;">${data.type}</span>`);
+  result.push(` <code>${call}</code>`);
+  result.push(`</${htag}>`);
+
+  // description
+  if (data.description) {
+    result.push(`<blockquote><p>${data.description}</p></blockquote>`);
+  }
+
+  // parameters
+  if (data.params && data.params.length > 0) {
+    result.push(`<blockquote>`);
+    result.push(`<p><strong>Parameters</strong></p>`)
+    result.push(`<ul>`);
+    data.params.forEach(param => {
+      const parsed = formatParam(param);
+      result.push(`<li>${parsed}</li>`);
+    });
+    result.push(`</ul>`);
+    result.push(`</blockquote>`);
+  }
+
+  // returns
+  if (data.returns && data.returns.length > 0) {
+    result.push(`<blockquote>`);
+    result.push(`<p><strong>Returns</strong></p>`)
+    result.push(`<ul>`);
+    data.returns.forEach(param => {
+      const parsed = formatParam(param);
+      result.push(`<li>${parsed}</li>`);
+    });
+    result.push(`</ul>`);
+    result.push(`</blockquote>`);
+  }
+
+  // nested
+  if (data.nested) {
+    Object.keys(data.nested).forEach((key) => {
+      cls = badgeClass(data.nested[key].type);
+      result.push(`<blockquote class="scoped ${cls}">`);
+      result.push(html(data.nested[key], true));
+      result.push(`</blockquote>`);
+    });
+  }
+
+  result.push('</div>');
+  return result.join('\n');
+}
+
+
+/**
+ * Read file with jsdoc and return data structure
+ * for formatting results. This method will automatically
+ * nest configuration for related modules (classes with methods, etc...)
+ *
+ * @param {String} path - Path to file.
+ */
+function read(path) {
+  const data = jsdoc.explainSync({
+    files: [path],
+  }).filter(item => item.comment);
+
+  const parsed = {};
+  data.map(item => {
+
+    // construct data from item
+    const obj = {
+      name: item.name,
+      type: item.kind,
+      description: item.classdesc || item.description,
+      line: item.meta.lineno,
+      path: item.meta.path,
+      filename: item.meta.filename,
+      returns: item.returns,
+      params: item.params,
+      nested: {},
+    };
+
+    // handle constructor methods
+    if (obj.name in parsed) {
+      parsed[obj.name].type = item.kind ? item.kind : obj.type;
+      parsed[obj.name].params = item.params ? item.params : obj.params;
+      parsed[obj.name].returns = item.returns ? item.returns : obj.returns;
+      parsed[obj.name].description = obj.description ? obj.description : item.description;
+    }
+
+    else if (item.memberof in parsed) {
+      parsed[item.memberof].nested[obj.name] = obj;
+    }
+
+    // save new base object
+    else {
+      parsed[item.name] = obj;
+    }
+
+  });
+
+  return parsed;
+}
+
+
+/**
+ * Markdown-it plugin for automatic code documentation
+ * using JSDoc3 conventions.
+ * @param md - Markdown object to extend.
+ * @param options - Options for plugin.
+ */
 export function autodoc(md, options) {
   options  = options || {};
   const regex = options.regex || /\/autodoc\s+(.+)$/;
+  const cache = {};
 
+  // add markdown-it rule for plugin
   md.core.ruler.push('autodoc', state => {
+
     let unwrap = [];
     state.tokens.forEach((token, idx) => {
+
+      // process inline tokens
       const match = token.content.match(regex);
       if (token.type === 'inline' && match) {
 
+        let [path, ...modules] = match[1].trim().split(/[ ,;]/);
+
+        // read data into cache
+        if (!(path in cache)) {
+          cache[path] = read(path);
+        }
+
+        // figure out modules to document
+        const data = cache[path];
+        if (!modules.length) {
+          modules = Object.keys(data);
+        }
+
         // render html for doc
-        const [path, ...modules] = match[1].trim().split(/[ ,;]/);
+        token.content = modules.map(key => html(data[key])).join('\n');
         token.type = 'html_inline';
-        token.content = document(path, modules);
         token.children = null;
 
         // unwrap outer tokens
@@ -68,61 +208,14 @@ export function autodoc(md, options) {
         }
       }
     });
+
+    // add extra token for autodoc css
+    const style = new Token('html_inline', '', 0);
+    style.content = CSS;
+    style.children = null;
+    state.tokens.push(style);
   });
 }
-
-// //
-// export function autodoc(md, options) {
-//   options  = options || {};
-//   const regex = options.regex || /\/autodoc\s+(.+)$/;
-//   md.use(Plugin(
-//     /autodoc/,
-//     function (match, utils) {
-//       console.log(match);
-//       const [path, ...modules] = match[1].trim().split(/[ ,;]/);
-//       return `<h1>${path}<h1><p>${modules}</p>`;
-//     }
-//   ));
-// }
-
-//
-//   function process(line) {
-//     const m = line.match(regex);
-//     const [path, ...modules] = m[1].trim().split(/[ ,;]/);
-//     return `
-//     <h1>${path}<h1>
-//     <p>${modules}</p>
-//     `;
-//   }
-//
-//   md.core.ruler.after('inline', 'autodoc', (state) => {
-//     state.tokens.forEach((block) => {
-//       if (block.type === 'inline' && block.children) {
-//         block.children.forEach((token) => {
-//           if (token.type === 'text' && regex.test(token.content)) {
-//               token.markdown = process(token.content);
-//           }
-//         });
-//       }
-//     });
-//     let token = new state.Token('text', '', 0);
-//     console.log(token);
-//     token.content = '<h1>test</h2>';
-//     state.tokens.push(token);
-//     return true;
-//   });
-//
-//   // md.core.textPostProcess.ruler.push('autodoc', {
-//   //   matcher: regex,
-//   //   onMatch: function(buffer, matches, state) {
-//   //     let token = new state.Token('text', '', 0);
-//   //     token.content = 'test' + matches[0];
-//   //     buffer.push(token);
-//   //   },
-//   // });
-//
-//   return md;
-// }
 
 
 // exports
@@ -130,10 +223,10 @@ export default function (options, ctx) {
   return {
     name: 'vuepress-autodoc',
 
-    async ready() {
-      // write css for plugin
-      // await context.writeTemp('plugins-autodoc.css', style)
-    },
+    // async ready() {
+    //   // write css for plugin
+    //   // await context.writeTemp('plugins-autodoc.css', style)
+    // },
 
     extendMarkdown(md) {
       return md.use(autodoc);
